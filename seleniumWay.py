df3a5c8ac6c970eda830809518a6a49b0ad0554c
manuelpacheco108 <manuelpm108@gmail.com>
2025-04-14 19:07:18 -0500

    Tuning the algorithm to visualize data

--- seleniumWay.py
+++ seleniumWay.py
@@ -12,8 +12,8 @@ driver.get('https://www.accuweather.com/en/co/medellin/107060/weather-forecast/1
 time.sleep(7)  # Adjust the sleep time as needed
 
 # Find elements and extract data
-def get_weather_data():
-    weather_data = []
+def get_weather_data() -> dict:
+    weather_data = {'hour': [], 'temperature': [], 'precipitation': []}
     while True:
         times = driver.find_elements(By.XPATH, '//div[@class="hourly-list__list-wrapper"]/div[@class="hourly-list__list"]/a[@class="hourly-list__list__item"]')
         for time in times:
@@ -22,33 +22,40 @@ def get_weather_data():
             precipitation = time.find_element(By.XPATH, './/div[@class="hourly-list__list__item-precip"]/span').text
             if hour == '':
                 return weather_data
-            weather_data.append({'hour': hour, 'temperature': temperature, 'precipitation': precipitation})
+            weather_data['hour'].append(hour)
+            weather_data['temperature'].append(temperature)
+            weather_data['precipitation'].append(precipitation)
         try:
             driver.find_element(By.CSS_SELECTOR, ".hourly-list__arrow-right").click()
             time.sleep(2)  # Wait for the new data to load
         except:
             break
+        print(weather_data)
     return weather_data
 
 # Function to show high precipitation probabilities and clothing advice
 def show_precipitation_advice(weather_data):
-    for data in weather_data:
-        hour = data['hour']
-        precipitation = int(data['precipitation'].replace('%', ''))
-        if precipitation > 75:
-            advice = "Lleva abrigo"
-        elif precipitation < 35:
-            advice = "No lleves abrigo"
-        else:
-            advice = "Considera llevar abrigo"
-        print(f'Hour: {hour}, Precipitation: {precipitation}%, Advice: {advice}')
+    hour = weather_data['hour']
+    precipitation_data = weather_data['precipitation']
+    temperature_data = weather_data['temperature']
+
+    rain = []
+    for i in range(len(precipitation_data)):
+        precipitation = int(precipitation_data[i].replace('%', ''))
+        if precipitation >= 60:
+            rain.append((hour[i], precipitation_data[i]))
+    print(weather_data)
+    if rain:
+        print("\n".join([f"{h}:00 con probabilidad de lluvia de {p}%" for h, p in rain]))
+    else:
+        print("Probablemente no llueva")
+    max_precipitation = max(precipitation_data, key=lambda x: int(x.replace('%', '')))
+    max_hour = hour[precipitation_data.index(max_precipitation)]
+    max_temperature = temperature_data[precipitation_data.index(max_precipitation)]
+    print(f"La probabilidad de lluvia más alta es {max_precipitation} en el horario {max_hour}:00 cuya temperatura es {max_temperature}")
 
 # Get all weather data
 all_weather_data = get_weather_data()
-
-# Print all data
-for data in all_weather_data:
-    print(f'Hour: {data["hour"]}, Temperature: {data["temperature"]}, Precipitation: {data["precipitation"]}')
 
 # Show precipitation advice
 show_precipitation_advice(all_weather_data)
